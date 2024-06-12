5188e107939c9d7fc7f47dc94a660f46eebff90d
JeonSeongWon <fpdlsnsqlc3@naver.com>
2024-06-12 17:45:47 +0900

    SDL 변환

--- main.cpp
+++ main.cpp
@@ -1,8 +1,23 @@
 #include "World.h"
+#include "SDL.h"
 #include "Engine.h"
 
-int main()
+int SDL_main(int argc, char* argv[])
 {
+	if (SDL_Init(SDL_INIT_EVERYTHING) < 0) 
+	{
+		std::cout << "Init Fail" << endl;
+	}
+	else
+	{
+		std::cout << "Init Success" << endl;
+	}
+
+	SDL_Window* MyWindow;
+	MyWindow = SDL_CreateWindow("My Game", 10, 10, 640, 480, SDL_WINDOW_OPENGL);
+
+
+	SDL_Renderer* MYRenderer = SDL_CreateRenderer(MyWindow, -1, SDL_RENDERER_ACCELERATED| SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_TARGETTEXTURE);
 
 	MYENGINE->LoadLevel("Map.txt");
 	MYENGINE->Run();
